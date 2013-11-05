dced664157cdb4a29bb6abc42f21651ec664c46a
Anisha Vasandani <anishavasandani@gmail.com>
2013-11-05 11:11:21 -0500

    adds tests user_spec.rb for username and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = Factory.build(:user)
+  end
+
+  it "should not be valid without a name" do 
+    @user.username = nil
+    @user.should_not be_valid
+  end
+
+  it "should not be valid without an email" do 
+    @user.email = nil
+    @user.should_not be_valid
+  end
+
+  it "should not be valid without an unique email address" do 
+    @user.email.uniq? = false
+    @user.should_not be_valid
+  end
 end
